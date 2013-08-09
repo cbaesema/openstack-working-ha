13315cd0ba0fcc08a1f7b1ce242750f3f5242498
root <root@build-server.eau.wi.charter.com>
2013-08-09 15:58:51 -0500

    modify setup

--- setup.sh
+++ setup.sh
@@ -16,29 +16,29 @@ cp /opt/grizzly-manifests/templates/* /etc/puppet/templates
 
 rm -rf /opt/puppet-augeas
 git clone http://github.com/camptocamp/puppet-augeas
-rm /usr/share/puppet/modules/augeas
+rm -rf /usr/share/puppet/modules/augeas
 mkdir /usr/share/puppet/modules/augeas
-cp /opt/puppet-augeas/* /usr/share/puppet/modules/augeas
+cp -r /opt/puppet-augeas/* /usr/share/puppet/modules/augeas
 
 rm -rf /opt/puppet-galera
 git clone http://github.com/danehans/puppet-galera
-rm /usr/share/puppet/modules/galera
+rm -rf /usr/share/puppet/modules/galera
 mkdir /usr/share/puppet/modules/galera
-cp /opt/puppet-galera/* /usr/share/puppet/modules/galera
+cp -r /opt/puppet-galera/* /usr/share/puppet/modules/galera
 
 
 rm -rf /opt/puppet-keepalived
 git clone http://github.com/CiscoSystems/puppet-keepalived
-rm /usr/share/puppet/modules/keepalived
+rm -rf /usr/share/puppet/modules/keepalived
 mkdir /usr/share/puppet/modules/keepalived
-cp /opt/puppet-keepalived/* /usr/share/puppet/modules/keepalived
+cp -r /opt/puppet-keepalived/* /usr/share/puppet/modules/keepalived
 
 
 rm -rf /opt/puppet-openstack-ha
 git clone http://github.com/danehans/puppet-openstack-ha
-rm /usr/share/puppet/modules/openstack-ha
+rm -rf /usr/share/puppet/modules/openstack-ha
 mkdir /usr/share/puppet/modules/openstack-ha
-cp /opt/puppet-openstack-ha/* /usr/share/puppet/modules/openstack-ha
+cp -r /opt/puppet-openstack-ha/* /usr/share/puppet/modules/openstack-ha
 
 
 cp /opt/openstack-working-ha/modules.list /etc/puppet/manifests
