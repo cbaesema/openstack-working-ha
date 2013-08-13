a42897d43215daab962a3bc3c812dc89df146150
root <root@build-server.eau.wi.charter.com>
2013-08-13 11:20:44 -0500

    add cherry picked modules

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ mkdir /usr/share/puppet/modules/augeas
 cp -r /opt/puppet-augeas/* /usr/share/puppet/modules/augeas
 
 rm -rf /opt/puppet-galera
-git clone http://github.com/CiscoSystems/puppet-galera
+git clone http://github.com/CiscoSystems/puppet-galera -b grizzly_wip
 rm -rf /usr/share/puppet/modules/galera
 mkdir /usr/share/puppet/modules/galera
 cp -r /opt/puppet-galera/* /usr/share/puppet/modules/galera
@@ -40,9 +40,28 @@ rm -rf /usr/share/puppet/modules/openstack-ha
 mkdir /usr/share/puppet/modules/openstack-ha
 cp -r /opt/puppet-openstack-ha/* /usr/share/puppet/modules/openstack-ha
 
-#cp /opt/openstack-working-ha/modules.list /etc/puppet/manifests
+#cherry picked patches
+rm -rf /opt/puppet-keystone
+rm -rf /usr/share/puppet/modules/keystone
+mkdir /usr/share/puppet/modules/keystone
+git clone http://github.com/stackforge/puppet-keystone -b stable/grizzly
+cd /opt/puppet-keystone
+git fetch https://review.openstack.org/stackforge/puppet-keystone refs/changes/05/39205/3 && git cherry-pick FETCH_HEAD
+cd /opt
+cp -r /opt/puppet-keystone/* /usr/share/puppet/modules/keystone
+
+
+
+rm -rf /opt/puppet-openstack
+rm -rf /usr/share/puppet/modules/openstack
+mkdir /usr/share/puppet/modules/openstack
+git clone http://github.com/stackforge/puppet-openstack -b stable/grizzly
+cd /opt/puppet-openstack
+git fetch https://review.openstack.org/stackforge/puppet-openstack refs/changes/31/39831/2 && git cherry-pick FETCH_HEAD
+git fetch https://review.openstack.org/stackforge/puppet-openstack refs/changes/87/40287/1 && git cherry-pick FETCH_HEAD
+cd /opt
+cp -r /opt/puppet-openstack/* /usr/share/puppet/modules/openstack
 
 cp /opt/openstack-working-ha/bin/* /usr/sbin
-
 
 
